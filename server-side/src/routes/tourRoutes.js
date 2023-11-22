e2ccfa5ca46c1d4999fa3fc8d7258248a1f3075c
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-11-22 17:04:57 +0600

    [Updated] tour booking

--- server-side/src/routes/tourRoutes.js
+++ server-side/src/routes/tourRoutes.js
@@ -10,16 +10,21 @@ const {
 	vehiclePayChart,
 	tourBooking,
 } = require("../controllers/tourControllers");
-const { requireSignIn } = require("../middlewares/authMiddlewares");
+const { requireSignIn, isAdmin } = require("../middlewares/authMiddlewares");
 const router = express.Router();
 
-router.post("/create-foodmenu", tourFoodMenu);
-router.post("/create-description", tourDescription);
-router.post("/create-tourTips", tourTips);
-router.post("/create-tourIncludeExclude", tourIncludeExclude);
-router.post("/create-tourOptions", tourPackageOptions);
-router.post("/create-personPay", personPayChart);
-router.post("/create-vehiclePay", vehiclePayChart);
+router.post("/create-foodmenu", requireSignIn, isAdmin, tourFoodMenu);
+router.post("/create-description", requireSignIn, isAdmin, tourDescription);
+router.post("/create-tourTips", requireSignIn, isAdmin, tourTips);
+router.post(
+	"/create-tourIncludeExclude",
+	requireSignIn,
+	isAdmin,
+	tourIncludeExclude
+);
+router.post("/create-tourOptions", requireSignIn, isAdmin, tourPackageOptions);
+router.post("/create-personPay", requireSignIn, isAdmin, personPayChart);
+router.post("/create-vehiclePay", requireSignIn, isAdmin, vehiclePayChart);
 
 router.post("/create-tourInfo", tourInfo);
 router.post(
