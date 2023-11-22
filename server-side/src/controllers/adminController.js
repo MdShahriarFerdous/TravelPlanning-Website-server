498b2b704c028a7058c2b42cbad66e28da2ee672
Mredul Kanthy Dhar <mreduldhar237@gmail.com>
2023-11-23 00:46:21 +0600

    [Created] update function for admin

--- server-side/src/controllers/adminController.js
+++ server-side/src/controllers/adminController.js
@@ -1,7 +1,7 @@
 const { comparePassword } = require("../helpers/hashPass");
 const { jwtSecretKey } = require("../../secrets");
 const { createJsonWebToken } = require("../helpers/jsonWebToken");
-const User = require("../models/userModel");
+const User = require("../models/usermodel/userModel");
 require("dotenv").config();
 
 
@@ -98,30 +98,29 @@ exports.getUserById = async (req, res, next) => {
     }
 };
 
-// delete user by id
-exports.deleteUserById = async (req, res, next) => {
+// update user by id
+exports.updateUserById = async (req, res, next) => {
     try {
-       
-        const {userId} = req.params;
+        const { userId } = req.params;
 
+        // find the user by id
         const user = await User.findById(userId);
 
+        // check if the user exists
         if (!user || user.isAdmin == true) {
             return res.status(404).json({ status: "error", message: "User not found" });
         }
 
-        // delete the user by ID
-        await User.findByIdAndDelete(userId);
+        // update the user (banned)
+        await User.findByIdAndUpdate(user._id, { isBanned: true });
 
-        res.status(200).json({ status: "success", message: "User deleted successfully" });
+        res.status(200).json({ status: "success", message: "User banned successfully" });
 
     } catch (error) {
-
-        next(error)
-        console.log(error.message)
-
+        next(error);
+        console.log(error.message);
     }
-}
+};
 
 
 
