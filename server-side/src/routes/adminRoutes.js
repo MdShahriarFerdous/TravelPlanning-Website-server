5fd0c58dd726e2d425ade88a0abfbadcce0abaad
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-11-26 09:11:09 +0600

    [Fixed] admin-route merge conflict

--- server-side/src/routes/adminRoutes.js
+++ server-side/src/routes/adminRoutes.js
@@ -8,25 +8,26 @@ router.post("/admin/login", adminController.adminLogin);
 
 // get all users
 router.get(
-  "/admin/get-all-users",
-  requireSignIn,
-  isAdmin,
-  adminController.getAllUsers
+	"/admin/get-all-users",
+	requireSignIn,
+	isAdmin,
+	adminController.getAllUsers
 );
 
 // get user by id
 router.get(
-  "/admin/get-user-by-id/:userId",
-  requireSignIn,
-  isAdmin,
-  adminController.getUserById
+	"/admin/get-user-by-id/:userId",
+	requireSignIn,
+	isAdmin,
+	adminController.getUserById
 );
 
 // update user by id
 router.put(
-  "/admin/ban-user/:userId",
-  requireSignIn,
-  adminController.updateUserById
+	"/admin/ban-user/:userId",
+	requireSignIn,
+	isAdmin,
+	adminController.updateUserById
 );
 
 module.exports = router;
