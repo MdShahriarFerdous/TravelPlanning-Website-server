308bb3a025c3eb9ac1c777c5cd3572fed906179b
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2023-12-05 00:21:00 +0600

    Modified: the destination controller

--- server-side/src/routes/DestinationRoute.js
+++ server-side/src/routes/DestinationRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { create, list, readById, update, remove } = require('../controllers/DestinationController');
+const { create, list, readById, update, remove, getTopDestinations} = require('../controllers/DestinationController');
 
 const router = express.Router();
 
 router.post('/destination', create);
 router.get('/destinations', list);
+router.get('/top-destinations', getTopDestinations);
 router.get('/destination/:id', readById);
 router.put('/destination/:id', update);
 router.delete('/destination/:id', remove);
