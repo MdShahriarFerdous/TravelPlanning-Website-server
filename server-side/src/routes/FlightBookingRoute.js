53a322972128870f64714574e085fc9c6504d1b8
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2023-12-16 20:28:27 +0600

    Change Flight Booking create functionality

--- server-side/src/routes/FlightBookingRoute.js
+++ server-side/src/routes/FlightBookingRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { create, list, readById, update, remove, cancelBooking} = require('../controllers/FlightBookingController');
+const {requireSignIn} = require("../middlewares/authMiddlewares");
 
 const router = express.Router();
 
-router.post('/flight-booking', create);
+router.post('/flight-booking', requireSignIn, create);
 router.get('/flight-bookings', list);
 router.get('/flight-booking/:id', readById);
 router.put('/flight-booking/:id', update);
