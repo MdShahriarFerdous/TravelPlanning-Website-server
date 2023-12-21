f4d80e967a44e2e8a686c4610258efa8284a3ec5
Hasib <hasibhosen7612@gmail.com>
2023-12-21 18:56:08 +0600

    Update itineraryController.js

--- server-side/src/controllers/itineraryController.js
+++ server-side/src/controllers/itineraryController.js
@@ -5,30 +5,18 @@ exports.addToWishlist = async (req, res, next) => {
     const userId = req.user._id;
     const { location_name, duration, latitude, longitude } = req.body;
 
-    // check if an itinerary exists for the user and location
-    const existingItinerary = await itineraryModel.findOne({
-      user: userId,
+    const result = await itineraryModel.create({
+      duration,
       location_name,
+      latitude,
+      longitude,
+      user: userId,
     });
-    if (existingItinerary) {
-      return res.status(200).json({
-        status: "success",
-        message: "Itinerary exists",
-      });
-    } else {
-      const result = await itineraryModel.create({
-        duration,
-        location_name,
-        latitude,
-        longitude,
-        user: userId,
-      });
 
-      return res.status(201).json({
-        status: "success",
-        message: result,
-      });
-    }
+    return res.status(201).json({
+      status: "success",
+      message: result,
+    });
   } catch (error) {
     next(error);
     console.error(error.message);
